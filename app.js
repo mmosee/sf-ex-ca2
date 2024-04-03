15326eee7c455c9d485a96ce6f588099bd6c81ab
mmosee <142989093+mmosee@users.noreply.github.com>
2024-04-03 14:23:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,33 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + "/index.html")
 });
 
+//
+app.post('/save', function(req, res) {
+    console.log('debug: /save');
+    return res.status(200).json({});
+});
+
+app.post('/publish', function(req, res) {
+    console.log('debug: /publish');
+    return res.status(200).json({});
+});
+
+app.post('/validate', function(req, res) {
+    console.log('debug: /validate');
+    return res.status(200).json({});
+});
+
+app.post('/stop', function(req, res) {
+    console.log('debug: /stop');
+    return res.status(200).json({});
+});
+
+app.post('/execute', function(req, res) {
+    console.log('debug: /execute');
+    return res.status(200).json({});
+});
+
+//
 app.listen(3000, () => {
     console.log("hi");
 })
