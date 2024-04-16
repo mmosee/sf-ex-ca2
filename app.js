03dcb0ff381e933b722dc369c0c7be1255c6e3c8
mmosee <nammosee@gmail.com>
2024-04-16 09:23:21 +0900

    500

--- app.js
+++ app.js
@@ -54,7 +54,9 @@ app.post('/publish', function(req, res) {
 app.post('/validate', function(req, res) {
     console.log('debug: /validate');
     
-    return res.status(200).json({});
+    // return res.status(400).json({});
+    return res.status(500).json({});
+    // return res.status(200).json({});
 });
 
 app.post('/stop', function(req, res) {
